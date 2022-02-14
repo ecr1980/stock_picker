4486f9a2db75a4869f256ddf6dbb2168a539b6fc
Eric Romano <ecr1980@gmail.com>
2022-02-14 13:08:53 -0500

    stock_picker method runs through array and returns the total profit for the most profitable trade.

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,8 +1,21 @@
 stock_array = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker(x)
-  best = Array.new
+  best = [0,0]
+  profit = 0
+  x.each_with_index do |element, index|
+    x.each_with_index do |elem_2, ind_2|
+      if ind_2 > index
+        sum = elem_2 - element
+        if sum > profit
+          profit = sum
+        end
+      end
+    end
+  end
   print best
+  print profit
 end
 
 stock_picker(stock_array)
+
